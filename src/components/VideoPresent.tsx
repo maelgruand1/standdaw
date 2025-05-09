c52759c07906d59087f8ab498eeb366967d12d86
Mael Gruand <maelgruand7@gmail.com>
2025-05-09 10:45:21 +0200

    Video config changed.

--- src/components/VideoPresent.tsx
+++ src/components/VideoPresent.tsx
@@ -4,7 +4,12 @@ export default function VideoPresent(): React.ReactElement {
     return (
         <div className="presentation">
             <h3>Video de presentación</h3>
-            <video className="video" src="./assets/daw-presentation.mp4" controls />
+                        
+            <video controls>
+                <source src="hhttps://github.com/maelgruand1/standdaw/blob/dev/public/assets/daw-presentation.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
         </div>
     );
 }
